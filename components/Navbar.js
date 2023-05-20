8861e72449f5828de3ee26934fc527670be52f5e
Sarah <sarahdavies186@gmail.com>
2023-05-20 16:18:24 +0100

    updates to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,79 +1,79 @@
+import React, { useState } from "react";
+
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <nav class="bg-white border-gray-200 dark:bg-gray-900 font-poppins-regular container">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="" class="flex items-center">
-            <span class="self-center text-2xl dark:text-white">
-              Mulih
-            </span>
-          </a>
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-            <svg
-              class="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+      <nav className="px-4 flex justify-end pt-4 w-full container">
+        <div className="flex-grow">Mulih</div>
+        <div
+          className={`duration-500 bg-white lg:static absolute md:min-h-fit ${
+            isMenuOpen ? "top-0" : "-top-full"
+          } md:w-auto w-full flex flex-col px-5`}
+        >
+          <div className="flex items-center h-full md:h-auto">
+            <ul className="flex flex-col items-start md:items-stretch md:flex-row gap-8">
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-green md:p-0"
-                  aria-current="page"
+                  href="/#about"
+                  className=""
                 >
-                  Home
+                 Home
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  href="/#team"
+                  className=""
+                  onClick={() => navigate("/#team")}
                 >
                   Properties
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  href="/#features"
+                  className=""
+                  onClick={() => navigate("/#features")}
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#features"
+                  className=""
+                  onClick={() => navigate("/#features")}
                 >
                   Services
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  href="/#features"
+                  className=""
+                  onClick={() => navigate("/#features")}
                 >
                   Contact
                 </a>
               </li>
             </ul>
+          </div>
+        </div>
+        <div className="flex items-center gap-6">
+          <div
+            onClick={() => onToggleMenu()}
+            className={`hamburger-menu cursor-pointer lg:hidden ${
+              isMenuOpen ? "open" : ""
+            }`}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
           </div>
         </div>
       </nav>
